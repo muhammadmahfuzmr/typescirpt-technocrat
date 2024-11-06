6be396427a212c23779c645e7d9a864849fa2e39
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-11-06 07:46:59 +0600

    ternary operator, nullish coalescing, optional chaining

---
+++ module1/src/1.10.ts
@@ -0,0 +1,42 @@
+{
+    // Union Type
+    type FrontendDeveloper = 'Fakibaz Developer' | 'Junior Developer';
+    type BackendDeveloper = 'Frontend Developer' | 'Expert Developer';
+    type FullstackDeveloper = FrontendDeveloper | BackendDeveloper;
+    const newDeveloper: FullstackDeveloper = 'Expert Developer'
+
+    type User = {
+        name: string;
+        age: number;
+        gender: 'Male' | 'Female';
+        contactNo?: string;
+        email?: string;
+        bloodGroup: 'A+' | 'O+' | 'AB+' | 'B+'
+    }
+
+    const user1: User = {
+        name: 'Mahfuz',
+        age: 22,
+        gender: 'Male',
+        bloodGroup: 'B+',
+        contactNo: '0152453222'
+
+    }
+    // intersection
+    type FontEndDeveloper = {
+        skills: string[];
+        expertsIn1: 'React';
+
+    }
+    type BackEndDeveloper = {
+        skills: string[];
+        expertsIn2: 'Typescript'
+    }
+    type FullStackDeveloper = FontEndDeveloper & BackEndDeveloper;
+    const fullStackDeveloper : FullStackDeveloper = {
+        skills: ['html', 'js', 'node'],
+        expertsIn1: 'React' ,
+        expertsIn2: 'Typescript'
+    }
+
+}
---
+++ module1/src/1.11.ts
@@ -0,0 +1,41 @@
+{
+    // ternary operator, nullish coalescing operator,optional chaining
+    const age = 15;
+    if (age > 18) {
+        // console.log('Adult')
+    } else {
+        //console.log('Teenager')
+    }
+
+
+    const isAdult = age > 18 ? 'Adult' : 'Teenager'
+    console.log({ isAdult })
+    // nullish coalescing operator
+    // nullish coalescing operator have to use ,if value is null or undefined and need to make decision.Othewise they retrun variable value.
+    const isAuthentication = 5;
+    const result1 = isAuthentication ?? 'Guest'
+    console.log(result1)
+
+    // Optional chaining ,optional type
+    type User = {
+        name: string;
+        address: {
+            city: string;
+            road?: string;
+            postalCode: number;
+            ps: string;
+            dist: string
+        }
+    }
+    const user: User = {
+        name: 'Mahfuz',
+        address: {
+            city: 'syl',
+            postalCode: 3374,
+            ps: 'Nabiganj',
+            dist: 'Habiganj'
+        }
+    }
+    const roadNo = user?.address?.road ?? 'Road Number is not available'
+    console.log(roadNo)
+}
