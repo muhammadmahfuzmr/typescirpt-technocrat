8139192b6894e7c3211713d591816d8e2f7ffe19
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-11-08 06:40:05 +0600

    constraints with generic

--- module2/2.6.ts
+++ module2/2.6.ts
@@ -11,6 +11,7 @@
     }
     const student1 = addCourseToStudent({id:125, name: 'Mahfuz', class: 12, email: 'lsl@gmail.com', })
     const student2 = addCourseToStudent({id:256, name: 'Bilal', email: 'bilal@gmail.com', class:11})
+    const student3 = addCourseToStudent({class: 25,id:125, email: 'mahfuz@gmail.com', name:'Mahfuz'})
 
     //
 }
