61624fcb58522cdbfef3235e98841581e5ef2931
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-11-25 07:11:28 +0600

    revision typescript basic data type

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -1,31 +1,39 @@
 // Basic Data Type
 
 // string
-let firstName : string = 'mahfuz'
+let firstName : string = 'mahfuz';
+let firstName1 : string = "Bilal" ;
 
 
 //number
-let roll: number = 125
+let roll: number = 125;
+let price: number = 300;
 
 //boolean
 let isAdmin : boolean = true ;
+let isPassed:boolean = true;
 
 // undefined
 let x : undefined = undefined
+let z : undefined = undefined;
 
 // null
 let y : null = null
+let w : null = null;
 
 
 // Array
 
-let friends : string[] = ['rachel', 'monica']
+let friends : string[] = ['rachel', 'monica'];
+let student: string[]  = ['Mahfuz', "Jawad", 'Bilal']
 
 let eligibleRollList : number[] = [1, 52,3]
+let marks : number[] = [12, 21, 36, 89];
 
 //tuple --> array --> order --> type of values
 let coordinates : [number, number] = [12, 36]
 coordinates[0] = 36
 console.log(coordinates)
-
+let credit: [number, number] = [1252, 36552]
+let priceOfPen : [number, string] = [10, "Pen"]
 let ageName : [number, string] = [22, 'mahfuz']
