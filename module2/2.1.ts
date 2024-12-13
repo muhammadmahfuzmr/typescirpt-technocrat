13d34fd393df48facc69c35f882c0ef6ff388622
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-12-13 07:29:42 +0600

    type assertion revise

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -1,36 +1,22 @@
 {
     //
     // type assertion
+    let anything: any;
+    anything = "Next level web development";
+    anything = 25;
+    (anything as number)
 
-    let anything: any
-    anything = 'next level web development';
-    //(anything as string).
-
-    const kgToGm = (value: string | number): number | undefined | string => {
-        if (typeof value === "string") {
-            const convertedValue = parseFloat(value) * 1000;
-            return `The Converted Value is ${convertedValue}`
-        }
-        else if (typeof value === 'number') {
-            const convertedValue = value * 1000;
-            return convertedValue
+    const kgToGm = (value: number | string): number | string | undefined => {
+        if (typeof value === "number") {
+            return value * 1000
+        } else if (typeof value === "string") {
+            const convertedValueToNumber = parseFloat(value)
+            const convertedValueToGm = convertedValueToNumber * 1000;
+            return `The converted Value is: ${convertedValueToGm}`
         }
     }
-
-    const result1 = kgToGm(1000) as number
-    const result2 = kgToGm('500') as string
-    console.log({ result1 }, { result2 })
-
-
-    type CustomError = {
-        massage: string
-    }
-    try {
-
-     }catch(error){
-        console.log(( error as CustomError).massage)
-     }
+const result1 = kgToGm(2500) as number;
+const result2  = kgToGm("2000") as string;
 
     //
-
 }
