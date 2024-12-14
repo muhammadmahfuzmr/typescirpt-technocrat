afa5b0d1adcb58d3cffe9889f069558572e57b47
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-12-14 08:04:50 +0600

    constraints revision

--- module2/2.6.ts
+++ module2/2.6.ts
@@ -1,4 +1,16 @@
 {
     //
+    // Constraints
+    const addToCourse =<T extends {id:number; name: string; email:string}> (student: T) => {
+        const course: string = "Next Level Web Development";
+        return {
+            ...student,
+            course
+        }
+    }
+
+const student1 = addToCourse({id:12, name: "Mahfuz", email:"mahfuz@gmail.com", expertsIn: "React"})
+const student2 = addToCourse({id:12, name: "Bilal", email:"bilal@gmail.com", hasWatch: true, })
+const student3 = addToCourse({id:12, name: "Jawad",email:"jawad@gmail.com"})
     //
 }
